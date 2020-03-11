eae6f157c9f7b5ebd092448f72939b180bb072f8
hallowf <alexandre.cortegaca@gmail.com>
2020-03-11 14:37:53 +0000

    Module 1

---
+++ Programming Concepts with Python - Revised/Module 1/hello_world.py
@@ -0,0 +1,7 @@
+#Retain backwards compatibility with python 2
+from __future__ import print_function
+print("Hello World!")
+
+# Write to system standard output
+import sys
+sys.stdout.write("Hello World!\n")
